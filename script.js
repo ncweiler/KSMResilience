b6d33fd3dd9c243399c77702bb83b0ca69154256
jthompson1216 <jthompson1216@gmail.com>
2014-03-14 16:48:32 -0400

    Added splashpage rollovers and jQuery and qtip
    Rollovers on main page for featured content. Added jQuery and qtip
    libraries.

---
+++ script.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+    $('#casestudies').each(function(){
+        $(this).qtip({
+            content: $(this).next('.tooltiptext'),
+            show: {
+              solo:true  
+            },
+            hide: {
+                fixed: true,
+                delay: 300
+            },  
+            position: {
+                my: 'left top',
+                at: 'right bottom',
+                target: $('#casestudies a img'),
+                adjust: {
+                    y:-250
+                }
+            },
+            style: {
+                classes: 'qtip-blue qtip-shadow'
+            }
+        });
+    });
+    
+     $('#risk').each(function(){
+        $(this).qtip({
+            content: $(this).next('.tooltiptext'),
+            show: {
+              solo:true  
+            },
+            hide: {
+                fixed: true,
+                delay: 300
+            },  
+            position: {
+                my: 'right top',
+                at: 'left bottom',
+                target: $('#risk a img'),
+                adjust: {
+                    y:-250
+                }
+            },
+            style: {
+                classes: 'qtip-blue qtip-shadow'
+            }
+        });
+    });
+    
+     $('#planning').each(function(){
+        $(this).qtip({
+            content: $(this).next('.tooltiptext'),
+            show: {
+              solo:true  
+            },
+            hide: {
+                fixed: true,
+                delay: 300
+            },  
+            position: {
+                my: 'left bottom',
+                at: 'right top',
+                target: $('#planning a img'),
+                adjust: {
+                    y:200
+                }
+            },
+            style: {
+                classes: 'qtip-blue qtip-shadow',
+            }
+        });
+    });
+    
+     $('#networks').each(function(){
+        $(this).qtip({
+            content: $(this).next('.tooltiptext'),
+            show: {
+              solo:true  
+            },
+            hide: {
+                fixed: true,
+                delay: 300
+            },  
+            position: {
+                my: 'right bottom',
+                at: 'left top',
+                target: $('#networks a img'),
+                adjust: {
+                    y:200
+                }
+            },
+            style: {
+                classes: 'qtip-blue qtip-shadow'
+            }
+        });
+    }); 
+});
