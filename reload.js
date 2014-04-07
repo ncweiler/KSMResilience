3d7166c973d1ba94e566dcb76592bb5d72077f6d
jthompson1216 <jthompson1216@gmail.com>
2014-04-07 10:11:43 -0400

    Added return to main page if idle

---
+++ reload.js
@@ -0,0 +1,21 @@
+idleTime = 0;
+$(document).ready(function () {
+    //Increment the idle time counter every minute.
+    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+
+    //Zero the idle timer on mouse movement.
+    $(this).mousemove(function (e) {
+        idleTime = 0;
+    });
+    $(this).keypress(function (e) {
+        idleTime = 0;
+    });
+});
+
+function timerIncrement() {
+    idleTime = idleTime + 1;
+    if (idleTime > 2) { // # should be 1 less than # of minutes you want to idle before going to home page
+        window.location.replace("index.html");
+    }
+}
+  
