73f2976e08365ebfab130c74f423e05875fb0aef
John Thompson <jthompson1216@gmail.com>
2014-04-22 10:07:24 -0500

    Adjusted idle time reload on 3_b
    Added new script file for the stop disasters page that increases the
    idle time before going back to the home page to 20 minutes.

---
+++ reload_stopdisasters.js
@@ -0,0 +1,25 @@
+//This javascript reverts the exhibit to the splash page after a certain amount of idle time (no keyboard/mouse input). 
+
+//To change idle time before returning to home page, change the number in line 21 (idleTime > n), where n is 1 less than the number of minutes you want to idle before reverting to the splash page.
+
+idleTime = 0;
+$(document).ready(function () {
+    //Increment the idle time counter every minute.
+    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+
+    //Zero the idle timer on mouse movement.
+    $(this).mousemove(function (e) {
+        idleTime = 0;
+    });
+    $(this).keypress(function (e) {
+        idleTime = 0;
+    });
+});
+
+function timerIncrement() {
+    idleTime = idleTime + 1;
+    if (idleTime > 19) { // # should be 1 less than # of minutes you want to idle before going to home page
+        window.location.replace("index.html"); //the link to go to after idle time, index.html is the homepage
+    }
+}
+  
